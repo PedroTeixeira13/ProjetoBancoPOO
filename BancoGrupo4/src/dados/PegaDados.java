6d7efd9ccb190082552e15bf1081e1f7f156b1a8
PedroTeixeira13 <pedrohcrt@gmail.com>
2023-09-07 21:10:44 -0300

    correção de bug

--- BancoGrupo4/src/dados/PegaDados.java
+++ BancoGrupo4/src/dados/PegaDados.java
@@ -29,16 +29,16 @@ public class PegaDados {
             if (linha != null) {
                 String [] divide = linha.split(",");
 
-                if(divide[2].equals(PessoaE.Diretor.name())) {
+                if(divide[2] == (PessoaE.Diretor.name())) {
                     Diretor d = new Diretor(divide[0], divide[1], divide[3], divide[2]);
                     listaPessoa.add(d);
-                } else if(divide[2].equals(PessoaE.Gerente.name())) {
+                } else if(divide[2] == (PessoaE.Gerente.name())) {
                     Gerente g = new Gerente(divide[0], divide[1], divide[3], divide[2]);
                     listaPessoa.add(g);
-                } else if(divide[2].equals(PessoaE.Presidente.name())) {
+                } else if(divide[2] == (PessoaE.Presidente.name())) {
                     Presidente p = new Presidente(divide[0], divide[1], divide[3], divide[2]);
                     listaPessoa.add(p);
-                } else if(divide[2].equals(PessoaE.Cliente.name())) {
+                } else if(divide[2] == (PessoaE.Cliente.name())) {
                     Cliente c = new Cliente(divide[0], divide[1], divide[3], divide[2]);
                     listaPessoa.add(c);
                 }
@@ -61,10 +61,10 @@ public class PegaDados {
             if(linha != null) {
                 String [] divide = linha.split(",");
 
-                if(divide[3].equals(ContaE.ContaCorrente.name())) {
+                if(divide[3] == (ContaE.ContaCorrente.name())) {
                     ContaCorrente cc = new ContaCorrente(divide[0], Double.parseDouble(divide[1]), Integer.parseInt(divide[2]), divide[3]);
                     listaConta.add(cc);
-                } else if(divide[3].equals(ContaE.ContaPoupanca.name())) {
+                } else if(divide[3] == (ContaE.ContaPoupanca.name())) {
                     ContaPoupanca cp = new ContaPoupanca(divide[0], Double.parseDouble(divide[1]), Integer.parseInt(divide[2]), divide[3]);
                     listaConta.add(cp);
                 }
